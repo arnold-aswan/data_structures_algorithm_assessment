1aeea95058bbb30dc1482a377101d92dd9b2d9c0
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-28 12:41:04 +0300

    implemented stacks

--- stacks.py
+++ stacks.py
@@ -1,27 +1,45 @@
+from collections import deque
 def is_balanced(expression):
-    hold = []
+    # hold = []
+    
+    # for char in expression:
+    #     if char in ["(","{","["]:
+    #         hold.append(char)
+    #     else:
+    #         if not hold:
+    #             return False
+    #         current_char = hold.pop()
+    #         if current_char == '(':
+    #             if char != ')':
+    #                 print(False)
+    #                 return False 
+    #             if current_char == '{':
+    #                 if char != "}":
+    #                     return False
+    #             if current_char == '[':
+    #                 if char != "]":
+    #                     return False
+    # if hold:
+    #     print(False)
+    #     return False
+    # print(True)
+    # return True      
+    
+    stack = deque()
+    opening_brackets = '({['
+    closing_brackets = ')}]'
+    bracket_pairs = {')':'(', '}':'{', ']':'['}
     
     for char in expression:
-        if char in ["(","{","["]:
-            hold.append(char)
-        else:
-            if not hold:
+        if char in opening_brackets:
+            stack.append(char)
+        elif char in closing_brackets:
+            if not stack:
                 return False
-            current_char = hold.pop()
-            if current_char == '(':
-                if char != ')':
-                    print(False)
-                    return False 
-                if current_char == '{':
-                    if char != "}":
-                        return False
-                if current_char == '[':
-                    if char != "]":
-                        return False
-    if hold:
-        print(False)
-        return False
-    print(True)
-    return True                   
+            if stack[-1] == bracket_pairs[char]:
+                stack.pop()
+    return not stack                
+                         
     
-is_balanced("([]{})")    
+print(is_balanced("([]") )  
+
